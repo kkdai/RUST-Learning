6f17ca9b169e625a73c4372e53345763e06d3193
Evan Lin <evanslin@gmail.com>
2014-12-25 13:20:55 +0800

    Add semver for more dependencies practice.

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,8 +1,20 @@
 extern crate color;
+extern crate semver;
+
 use color::{Rgb, ToHsv};
+use semver::Version;
 
 fn main() {
   println!("Create RGB from HSV");
   let red = Rgb::new(255u8, 0, 0);
   println!("HSV {}", red.to_hsv::<f32>());
+
+  assert!(Version::parse("1.2.3") == Ok(Version {
+  	major: 1u,
+  	minor: 2u,
+  	patch: 3u,
+  	pre: vec!(),
+  	build: vec!(),
+  }));
+  println!("Version comparision success!");
 }
